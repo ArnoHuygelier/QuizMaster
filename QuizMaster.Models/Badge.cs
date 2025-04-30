c8a2a24a1533ce96ba8e3dfa3c271576f7a9dca6
32465 <32465@GILLES>
2025-04-30 19:06:32 +0800

    Models added

---
+++ QuizMaster.Models/Badge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizMaster.Models
+{
+	public partial class Badge
+	{
+		[Key]
+		[Column("BadgeID")]
+		public int BadgeId { get; set; }
+
+		[StringLength(200)]
+		public string BadgeName { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public string Criteria { get; set; } = null!;
+
+		[InverseProperty("Badge")]
+		public virtual ICollection<UserBadge> UserBadges { get; set; } = new List<UserBadge>();
+	}
+
+}

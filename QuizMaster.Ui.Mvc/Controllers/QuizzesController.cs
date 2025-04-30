7a0ec879b7d2f0a32a992038614b50a26a52e8cc
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-04-30 13:07:30 +0200

    Add QuizzesController

---
+++ QuizMaster.Ui.Mvc/Controllers/QuizzesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizMaster.Ui.Mvc.Controllers
+{
+    public class QuizzesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

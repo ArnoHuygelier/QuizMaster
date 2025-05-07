0bbb046c885927b88e9e04c5bf509c7ab244c3e2
ArnoHuygelier <arno2003.huygelier1@gmail.com>
2025-05-07 11:02:34 +0200

    fixing

--- QuizMaster.Services/QuizService.cs
+++ QuizMaster.Services/QuizService.cs
@@ -12,27 +12,42 @@ namespace QuizMaster.Services
     public class QuizService
     {
         private readonly QuizMasterDbContext _dbContext;
-
         public QuizService(QuizMasterDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-
-        public IList<Quiz> GetAll()
+        public IList<Quiz> Find() /// Gets all quizzes
         {
             return _dbContext.Quizzes.Include(q => q.CreatedByNavigation).ToList();
         }
-
-        public Quiz? GetById(int id)
+        public Quiz? Get(int id) /// Gets a specific quiz by id
         {
             return _dbContext.Quizzes.FirstOrDefault(q => q.QuizId == id);
         }
-
-        public Quiz? Create(Quiz quiz)
+        public Quiz? Create(Quiz quiz) /// Creates a new quiz
         {
             _dbContext.Quizzes.Add(quiz);
             _dbContext.SaveChanges();
             return quiz;
+        }
+        public Quiz? Update(int id, Quiz quiz) /// Updates a specific quiz by id
+        {
+            var tempQuiz = Get(id);
+
+            if(tempQuiz is null)
+            {
+                return null;
+            }
+
+            tempQuiz.Title = quiz.Title;
+            tempQuiz.Description = quiz.Description;
+            tempQuiz.Questions = quiz.Questions;
+            tempQuiz.CreatedBy = quiz.CreatedBy;
+            tempQuiz.CreatedByNavigation = quiz.CreatedByNavigation;
+
+            _dbContext.SaveChanges();
+            return tempQuiz;
+
         }
     }
 }

2aa89090a3453fae0ab950a8d18e3e65a3d3ce08
jarne10125 <popelierjarne@hotmail.com>
2025-05-08 13:37:36 +0200

    Badges pages done, user pages done

--- QuizMaster.Ui.Mvc/Controllers/DummyController.cs
+++ QuizMaster.Ui.Mvc/Controllers/DummyController.cs
@@ -96,4 +96,14 @@ public class DummyController : Controller
         return View();
     }
 
+    public IActionResult EditUser()
+    {
+        return View();
+    }
+
+    public IActionResult CreateBadge()
+    {
+        return View();
+    }
+
 }

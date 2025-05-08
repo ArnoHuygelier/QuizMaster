b1a35923834b275bfb2cb27d049b2d8781e17b08
jarne10125 <popelierjarne@hotmail.com>
2025-05-08 15:22:57 +0200

    Removed property from model

--- QuizMaster.Models/Quiz.cs
+++ QuizMaster.Models/Quiz.cs
@@ -13,8 +13,7 @@ namespace QuizMaster.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string? ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }      
         public int UserId { get; set; }
         public User User { get; set; }
 

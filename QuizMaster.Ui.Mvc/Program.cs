6045a5c81d16aa96310b2f819c338e6e3210fe3d
32465 <32465@GILLES>
2025-05-08 19:42:39 +0800

    Fixed login, you can now login correctly.

--- QuizMaster.Ui.Mvc/Program.cs
+++ QuizMaster.Ui.Mvc/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<QuizMasterDbContext>(options =>
 });
 
 builder.Services.AddDefaultIdentity<User>(options => 
-     options.SignIn.RequireConfirmedAccount = false)
+     options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
 	.AddEntityFrameworkStores<QuizMasterDbContext>();
 

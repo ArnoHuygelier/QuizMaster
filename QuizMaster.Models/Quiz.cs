663f309fe64bbf55d72c9904afe1d329f88ff2e3
jarne10125 <popelierjarne@hotmail.com>
2025-05-08 15:13:27 +0200

    Quiz create

--- QuizMaster.Models/Quiz.cs
+++ QuizMaster.Models/Quiz.cs
@@ -14,7 +14,7 @@ namespace QuizMaster.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
-
+        public string? ImageUrl { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
 

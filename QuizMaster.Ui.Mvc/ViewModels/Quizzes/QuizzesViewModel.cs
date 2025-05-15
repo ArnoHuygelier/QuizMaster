0e2d59e87a88b7ce7994f7f134b211048b8590a5
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-05-15 15:30:27 +0200

    Update quizzescontroller

---
+++ QuizMaster.Ui.Mvc/ViewModels/Quizzes/QuizzesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using QuizMaster.Models;
+
+namespace QuizMaster.Ui.Mvc.ViewModels.Quizzes
+{
+    public class QuizzesViewModel
+    {
+        public int Id { get; set; }
+        public List<Quiz> Quizzes { get; set; } = new List<Quiz>();
+    }
+}

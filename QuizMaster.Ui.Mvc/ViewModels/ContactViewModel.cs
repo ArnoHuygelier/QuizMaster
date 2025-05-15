c0eae636492ae10e78b8096f1d257a12d6f0c169
32465 <32465@GILLES>
2025-05-15 18:00:08 +0800

    Refactored header/footer - Added contact, about, tos, privacy page

---
+++ QuizMaster.Ui.Mvc/ViewModels/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMaster.Ui.Mvc.ViewModels
+{
+
+	public class ContactViewModel
+	{
+		[Required(ErrorMessage = "Name is required")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Email is required")]
+		[EmailAddress(ErrorMessage = "Invalid Email")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Subject is required")]
+		public string Subject { get; set; }
+
+		[Required(ErrorMessage = "Message is required")]
+		public string Message { get; set; }
+	}
+}

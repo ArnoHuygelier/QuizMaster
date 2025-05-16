041e877268c2b4b1b2f4958ae9b4e39de3496b98
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-05-16 17:17:52 +0200

    Updated view to work with helper

---
+++ QuizMaster.Ui.Mvc/Helpers/ImageHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizMaster.Ui.Mvc.Helpers
+{
+    public static class ImageHelper
+    {
+        public static string GetQuizImageUrl(string? imageUrl)
+        {
+            var fileName = string.IsNullOrWhiteSpace(imageUrl) ? "defaultQuiz.png" : imageUrl;
+            return $"/images/quizimage/{fileName}";
+        }
+    }
+}

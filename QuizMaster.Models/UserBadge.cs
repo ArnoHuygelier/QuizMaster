6da680edad1cab0467673b5c34b28718c7520fec
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-05-16 10:17:58 +0200

    Refactored database models

--- QuizMaster.Models/UserBadge.cs
+++ QuizMaster.Models/UserBadge.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuizMaster.Models
 {
+    [Table(nameof(UserBadge))]
     public class UserBadge
     {
         [Key]

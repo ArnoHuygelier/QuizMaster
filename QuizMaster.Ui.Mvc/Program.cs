305331a83cca564e46dd7bbaededb310d6e91057
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-05-21 13:13:52 +0200

    Update play view to change timer color

--- QuizMaster.Ui.Mvc/Program.cs
+++ QuizMaster.Ui.Mvc/Program.cs
@@ -79,7 +79,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=quizzes}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 // Seed the database with roles

11d90a287dee89c86add7018697ae72d0c44655a
cheynskjento <cheynskjent@gmail.com>
2025-05-21 11:03:07 +0200

    Quiz gameplay fixed

--- QuizMaster.Ui.Mvc/ViewModels/Game/AnswerSubmissionViewModel.cs
+++ QuizMaster.Ui.Mvc/ViewModels/Game/AnswerSubmissionViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace QuizMaster.Ui.Mvc.ViewModels.Game
-
 {
     public class AnswerSubmissionViewModel
     {
@@ -11,8 +10,15 @@ namespace QuizMaster.Ui.Mvc.ViewModels.Game
         [Required]
         public int QuestionId { get; set; }
 
-        [Required]
-        public int SelectedAnswerId { get; set; }
-    }
+        public int? SelectedAnswerId { get; set; }
 
+        [Required]
+        public int CorrectCount { get; set; }
+
+        [Required]
+        public int CurrentIndex { get; set; }
+
+        [Required]
+        public bool IsTimedOut { get; set; }
+    }
 }

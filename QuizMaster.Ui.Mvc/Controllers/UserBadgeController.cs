6043a473b1344382d002ab0a96e465aa9ac77d7f
32465 <32465@GILLES>
2025-05-23 17:45:37 +0800

    Finished 'MyBadges'

--- QuizMaster.Ui.Mvc/Controllers/UserBadgeController.cs
+++ QuizMaster.Ui.Mvc/Controllers/UserBadgeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using QuizMaster.Models;
 using QuizMaster.Services;
 
 namespace QuizMaster.Ui.Mvc.Controllers
 {
+	[Authorize]
 	public class UserBadgeController : Controller
 	{
 		private readonly UserBadgeService _badgeService;
@@ -18,15 +20,16 @@ namespace QuizMaster.Ui.Mvc.Controllers
 			_userManager = userManager;
 		}
 
-		public async Task<IActionResult> MyBadges(string userId)
+		public async Task<IActionResult> MyBadges()
 		{
-			var badgesByUserId = await _badgeService.GetUserBadgesByUserId(userId);
-			if (badgesByUserId == null)
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
 			{
-				return NotFound();
+				return Challenge();
 			}
+
+			var badgesByUserId = await _badgeService.GetUserBadgesByUserId(user.Id);
 			return View(badgesByUserId);
 		}
-
 	}
 }

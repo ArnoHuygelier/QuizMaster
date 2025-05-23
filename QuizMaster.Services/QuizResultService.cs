9d65229bdb20e9d19a1551b8810c01129f0fd568
32465 <32465@GILLES>
2025-05-23 18:04:02 +0800

    Added My Quizzes page (QuizResult service/controller)

---
+++ QuizMaster.Services/QuizResultService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizMaster.Models;
+using QuizMaster.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaster.Services
+{
+	public class QuizResultService
+	{
+		private readonly QuizMasterDbContext _context;
+
+		public QuizResultService(QuizMasterDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<ICollection<QuizResult>> Find()
+		{
+			return await _context.QuizResults
+				.Include(u => u.User)
+				.Include(q => q.Quiz)
+				.ToListAsync();
+		}
+
+		public async Task<ICollection<QuizResult>> GetQuizResultsByUserId(string userId)
+		{
+			return await _context.QuizResults
+				.Include(u => u.User)
+				.Include(b => b.Quiz)
+				.Where(ub => ub.UserId == userId)
+				.ToListAsync();
+		}
+
+		public async Task<QuizResult?> Get(int id)
+		{
+			return await _context.QuizResults
+				.Include(u => u.User)
+				.Include(b => b.Quiz)
+				.FirstOrDefaultAsync(ub => ub.Id == id);
+		}
+	}
+}
---
+++ QuizMaster.Ui.Mvc/Controllers/QuizResultController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuizMaster.Models;
+using QuizMaster.Services;
+
+namespace QuizMaster.Ui.Mvc.Controllers
+{
+	[Authorize]
+	public class QuizResultController : Controller
+	{
+		private readonly QuizResultService _quizResultService;
+		private readonly UserManager<User> _userManager;
+
+		public QuizResultController(QuizResultService quizResultService, UserManager<User> userManager)
+		{
+			_quizResultService = quizResultService;
+			_userManager = userManager;
+		}
+
+		public async Task<IActionResult> MyQuizzes()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return Challenge();
+			}
+
+			var badgesByUserId = await _quizResultService.GetQuizResultsByUserId(user.Id);
+			return View(badgesByUserId);
+		}
+	}
+}

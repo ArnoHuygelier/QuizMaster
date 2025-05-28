a1d2d619c454f14424feceb8f5d2bdf5d1edc6be
LeanderRoelandtVives <leander.roelandt@student.vives.be>
2025-05-28 16:51:17 +0200

    started full-stack for the playerdeatils page in leaderboard

--- QuizMaster.Ui.Mvc/Controllers/LeaderboardController.cs
+++ QuizMaster.Ui.Mvc/Controllers/LeaderboardController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using QuizMaster.Models;
 using QuizMaster.Services;
 using QuizMaster.Ui.Mvc.ViewModels.Leaderboard;
+using System.Threading.Tasks;
 
 namespace QuizMaster.Ui.Mvc.Controllers
 {
     public class LeaderboardController : Controller
     {
         private readonly LeaderboardService _leaderboardService;
+        private readonly QuizResultService _quizResultService;
 
-        public LeaderboardController(LeaderboardService leaderboardService)
+        public LeaderboardController(LeaderboardService leaderboardService, QuizResultService quizResultService)
         {
             _leaderboardService = leaderboardService;
+            _quizResultService = quizResultService;
         }
+
 
         public IActionResult Index()
         {
@@ -31,6 +38,21 @@ namespace QuizMaster.Ui.Mvc.Controllers
             }).ToList();
 
             return View(leaderboardViewModels);
+        }
+
+
+        public async Task<IActionResult> PlayerDetails([FromBody] string id)
+        {
+            var quizResults = await _quizResultService.GetQuizResultsByUserId(id);
+
+            List<PlayerDetailsViewModel> playerDetails = quizResults.Select(x => new PlayerDetailsViewModel
+            {
+                Title = x.Quiz.Title,
+                Score = x.Score,
+                SubmittedAt = x.SubmittedAt
+            }).ToList();
+
+            return View(playerDetails);
         }
     }
 }
